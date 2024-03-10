d2828721a57811582c78a6dc72f220facf7f348f
quentinformatique <quentin161204@gmail.com>
2024-03-10 23:32:42 +0100

    add comments

--- RLautoTraining/RLautoTraining.cpp
+++ RLautoTraining/RLautoTraining.cpp
@@ -9,7 +9,7 @@ BAKKESMOD_PLUGIN(RLautoTraining, "Auto Training Plugin", "1.0", PERMISSION_ALL)
 
 void RLautoTraining::onLoad()
 {
-	this->Log("Plugin: RLautoTraining loaded !");
+	this->Log("RLautoTraining made by quentinformatique");
 	this->loadHooks();
 }
 void RLautoTraining::onUnload()
@@ -19,6 +19,7 @@ void RLautoTraining::onUnload()
 
 void RLautoTraining::loadHooks()
 {
+	// game events
 	gameWrapper->HookEvent("Function TAGame.GameEvent_Soccar_TA.EventMatchEnded", std::bind(&RLautoTraining::GameEndedEvent, this, std::placeholders::_1));
 	gameWrapper->HookEvent("Function TAGame.AchievementManager_TA.HandleMatchEnded", std::bind(&RLautoTraining::GameEndedEvent, this, std::placeholders::_1));
 
@@ -26,6 +27,7 @@ void RLautoTraining::loadHooks()
 
 void RLautoTraining::GameEndedEvent(string name) 
 {
+	// command to launch you back into freeplay
 	cvarManager->executeCommand("load_freeplay");
 }
 
